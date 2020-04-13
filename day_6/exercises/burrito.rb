f0af80a4515c40851d2eafab7f2e79529d8e37bf
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-04-12 21:32:02 -0400

    :sparkles: Add day 6 exercises
    Add day 6 exercises

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -1,15 +1,29 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
 # 3. change_protein
 
 class Burrito
-  attr_reader :protein, :base, :toppings
+  attr_reader :base
+  attr_accessor :protein, :toppings
+
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
     @toppings = toppings
+  end
+
+  def add_topping(topping)
+    @toppings << topping
+  end
+
+  def remove_topping(topping)
+    @toppings.delete(topping)
+  end
+
+  def change_protein(protein)
+    @protein = protein
   end
 end
 
@@ -17,3 +31,9 @@ dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+dinner.add_topping("fajita veggies")
+p dinner.toppings
+dinner.remove_topping("cheese")
+p dinner.toppings
+p dinner.change_protein("Tofu")
+p "A burrito with #{dinner.protein}, #{dinner.base}, #{dinner.toppings[0]}, #{dinner.toppings[1]}, and #{dinner.toppings[2]}. Yum!"
--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -1,5 +1,30 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+  attr_reader :name, :age, :mood
+
+  def initialize(name, age, mood = "content")
+    @name = name
+    @age  = age
+    @mood = mood
+  end
+
+  def have_birthday
+    puts "Happy birthday, #{name}!"
+    @age += 1
+  end
+
+  def dance
+    @mood = "ecstatic!"
+  end
+
+end
+
+naftali = Person.new("Naftali", 34)
+p naftali.mood
+naftali.have_birthday
+p naftali.age
+naftali.dance
+p naftali.mood
